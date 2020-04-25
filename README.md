33cbbe88f37a366c27b406eb024fb1c5d54f2f2f
p-karol <priscilla.karolczak@gmail.com>
2020-04-25 17:58:25 +0100

    Implementada classe setStatus

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ContaMagica
 Tarefa Teste Unitário
+
+Casos de teste (com indicativo da técnica utilizada), 
+
+Relatórios de defeitos (até que todos foram eliminados)
+
+Análise de cobertura do código
+
+Novos casos de teste acrescentados (com justificativa) ou justificativa para a não necessidade de acrescentar casos de teste.
--- src/main/java/com/contamagica/ContaMagica.java
+++ src/main/java/com/contamagica/ContaMagica.java
@@ -19,7 +19,32 @@ public class ContaMagica {
     public int getStatus(){
         return this.status;
     };
-	
+    
+    public void setStatus(int operacao) { 
+
+        switch(this.getStatus()){
+            case this.SILVER:
+            break;
+
+            case this.GOLD:
+            break;
+
+            case this.PLATINUM:
+            break;
+        } 
+
+        if ((this.getSaldo() >= 50000 && this.getStatus() == this.SILVER) || (this.getSaldo() < 100000 && this.getStatus() == this.PLATINUM)) { 
+            this.status = this.GOLD; 
+        } else if (this.getSaldo() >= 200000 && this.getStatus() == this.GOLD) { 
+            this.status = this.PLATINUM; 
+        } else if (this.getSaldo() < 25000 && this.getStatus() == this.GOLD) { 
+            this.status = this.SILVER; 
+        } else { 
+        
+        this.status = this.getStatus(); 
+        
+    } 
+
     //void deposito(int valor) throws INVALID_OPER_EXCEPTION;
 
     //void retirada(int valor) throws INVALID_OPER_EXCEPTION;
